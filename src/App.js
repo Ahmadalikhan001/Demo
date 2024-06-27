e03b79728a8f1047413eb906afde0a832fcb7f89
Ahmad Ali Khan <ahmadalik282@gmail.com>
2024-06-27 14:47:33 +0500

    Set a very Simple Web Page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-
-import React, { useState } from 'react';
 import './App.css';
-
+let name = "Ahmad Ali Khan"
 function App() {
-  const [text, setText] = useState('Press a button');
-
-  const handleClick = (buttonNumber) => {
-    if (buttonNumber === 1) {
-      setText('Button 1 pressed');
-    } else if (buttonNumber === 2) {
-      setText('Button 2 pressed');
-    }
-  };
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <div id="text">{text}</div>
-        <button onClick={() => handleClick(1)}>Button 1</button>
-        <button onClick={() => handleClick(2)}>Button 2</button>
-      </header>
-    </div>
+    <>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+      <div className='contaner'>
+        <h1>Hello {name}</h1>
+      <nav>
+      </nav>
+      </div>
+    </>
   );
 }
 
